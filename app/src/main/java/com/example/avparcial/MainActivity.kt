9789811751a9532c69d423976db35fc371986220
marcelemrofka <marceleeduarda3@gmail.com>
2024-10-02 21:50:37 -0300

    nova tela main

--- app/src/main/java/com/example/avparcial/MainActivity.kt
+++ app/src/main/java/com/example/avparcial/MainActivity.kt
@@ -2,71 +2,74 @@ package com.example.avparcial
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.avparcial.databinding.ActivityMainBinding
+import com.example.avparcial.databinding.ActivityTesteBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTesteBinding
 
-    private lateinit var binding: ActivityMainBinding
+    // Email e senha fixos
+    private val emailLogin = "parcial@unaerp.com"
+    private val senhaLogin = "Aplicativo123@"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityTesteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        // Ajustes nas barras de sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        binding.buttonAcessar.setOnClickListener {
-            val email = binding.emailusuario.text.toString().trim()
-            val senha = binding.SenhaUsuario.text.toString().trim()
+        // Clique no botão "Acessar"
+        binding.Acessar.setOnClickListener {
+            val email = binding.email.text.toString().trim()
+            val senha = binding.senha.text.toString().trim()
 
-            // Verificações de email e senha
+            // Verificações de email e senha fixos
             if (email.isEmpty() || senha.isEmpty()) {
                 Snackbar.make(findViewById(android.R.id.content), "Todos os campos devem ser preenchidos.", Snackbar.LENGTH_LONG).show()
             } else if (!verificaEmail(email)) {
                 Snackbar.make(findViewById(android.R.id.content), "Email inválido.", Snackbar.LENGTH_LONG).show()
-            } else if (!VerificaSenha(senha)) {
+            } else if (!verificaSenha(senha)) {
                 Snackbar.make(findViewById(android.R.id.content), "A senha deve conter pelo menos uma letra, um número e um caractere especial.", Snackbar.LENGTH_LONG).show()
+            } else if (email != emailLogin || senha != senhaLogin) {
+                Snackbar.make(findViewById(android.R.id.content), "Email ou senha incorretos.", Snackbar.LENGTH_LONG).show()
             } else {
-                // ok, navegue para a próxima tela
                 val intent = Intent(this, SuasListas::class.java)
                 startActivity(intent)
             }
         }
-        binding.buttonCriarConta.setOnClickListener {
+
+        // Clique no botão "Cadastrar"
+        binding.Cadastrar.setOnClickListener {
             val intent = Intent(this, criar_conta::class.java)
             startActivity(intent)
         }
-        binding.teste.setOnClickListener{
-            val intent = Intent(this, activity_teste::class.java)
-            startActivity(intent)
-        }
-
     }
 
-    // VERIFICA EMAIL
+    // Verifica se o email é válido
     private fun verificaEmail(email: String): Boolean {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() && email.length in 3..254
     }
 
-    // VERIFICA SENHA
-    private fun VerificaSenha(password: String): Boolean {
+    private fun verificaTamanhoEmail(email: String): Boolean {
+        return email.length in 3..254
+    }
+
+
+    // Verifica se a senha é válida
+    private fun verificaSenha(password: String): Boolean {
         val hasLetter = password.any { it.isLetter() }
         val hasDigit = password.any { it.isDigit() }
         val hasSpecialChar = password.any { !it.isLetterOrDigit() }
         return hasLetter && hasDigit && hasSpecialChar && (password.length >= 8)
     }
-
-
 }
--- app/src/main/java/com/example/avparcial/criar_conta.kt
+++ app/src/main/java/com/example/avparcial/criar_conta.kt
@@ -14,6 +14,7 @@ class criar_conta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_criar_conta)
+
     }
 }
 
@@ -59,8 +60,7 @@ class criar_conta : AppCompatActivity() {
 //                val intent = Intent(this, SuasListas::class.java)
 //                startActivity(intent)
 //            }
-//        }
-//    }
+//        } }
 //
 //    // VERIFICA NOME
 //    private fun isValidName(name: String): Boolean {
