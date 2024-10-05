9be04064c12a379f566f043e96b8f84366659331
isabertocco05 <111938842+isabertocco05@users.noreply.github.com>
2024-10-05 09:33:50 -0300

    Deleção tela de Teste

--- app/src/main/java/com/example/avparcial/MainActivity.kt
+++ app/src/main/java/com/example/avparcial/MainActivity.kt
@@ -5,11 +5,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.avparcial.databinding.ActivityTesteBinding
+import com.example.avparcial.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityTesteBinding
+    private lateinit var binding: ActivityMainBinding
 
     // Email e senha fixos
     private val emailLogin = "parcial@unaerp.com"
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityTesteBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // Ajustes nas barras de sistema
